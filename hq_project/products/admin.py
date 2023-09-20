894d691d627cb30828215cd9fe7fc100786720c2
LemonCat <ilnaz.imamov77@gmail.com>
2023-09-20 23:37:36 +0300

    Изменение базы данных

--- hq_project/products/admin.py
+++ hq_project/products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Product, ProductLesson, Access
+from .models import *
 
 
 admin.site.register(Product)
-admin.site.register(ProductLesson)
+# admin.site.register(ProductLesson)
 admin.site.register(Access)
--- hq_project/products/models.py
+++ hq_project/products/models.py
@@ -5,19 +5,20 @@ from lessons.models import Lesson
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lessons = models.ManyToManyField(Lesson)
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
     
 
-class ProductLesson(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+# class ProductLesson(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = 'Продукт-Урок'
-        verbose_name_plural = 'Продукты-Уроки'
+#     class Meta:
+#         verbose_name = 'Продукт-Урок'
+#         verbose_name_plural = 'Продукты-Уроки'
 
 
 class Access(models.Model):
